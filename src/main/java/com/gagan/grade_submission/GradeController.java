777449a19ef81347012d51d16e43163522f55711
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-13 23:20:32 -0500

    level 1

---
+++ src/main/java/com/gagan/grade_submission/GradeController.java
@@ -0,0 +1,17 @@
+package com.gagan.grade_submission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradeController {
+
+    @GetMapping("/grades")
+    public String getGrades(Model model){
+        Grade grade=new Grade("Gagan","Potions","C-");
+        model.addAttribute("grade",grade);
+        return "grades";
+    }
+    
+}
---
+++ src/main/java/com/gagan/grade_submission/GradeSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.gagan.grade_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+}
