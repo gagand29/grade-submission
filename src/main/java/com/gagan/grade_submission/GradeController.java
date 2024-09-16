1c4da06783c26f842eafb317358363ab22117865
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-16 15:26:27 -0500

    linked stylesheet using thymeleaf

--- src/main/java/com/gagan/grade_submission/GradeController.java
+++ src/main/java/com/gagan/grade_submission/GradeController.java
@@ -3,15 +3,26 @@ package com.gagan.grade_submission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class GradeController {
 
+    // Initialize the list with Arrays.asList
+    List<Grade> StudentGrades = new ArrayList<>(Arrays.asList(
+            new Grade("Gagan", "Potions", "C-"),
+            new Grade("Anshuman", "Potions", "B-"),
+            new Grade("Megha", "Potions", "Z-")
+    ));
+
     @GetMapping("/grades")
-    public String getGrades(Model model){
-        Grade grade=new Grade("Gagan","Potions","C-");
-        model.addAttribute("grade",grade);
+    public String getGrades(Model model) {
+        
+      
+
+        model.addAttribute("grades", StudentGrades);
         return "grades";
     }
-    
 }
