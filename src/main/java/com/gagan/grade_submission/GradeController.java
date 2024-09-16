0f5aa162c819a75f75e384cfec4788448e6385c1
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-16 16:03:15 -0500

    form

--- src/main/java/com/gagan/grade_submission/GradeController.java
+++ src/main/java/com/gagan/grade_submission/GradeController.java
@@ -6,21 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class GradeController {
 
     // Initialize the list with Arrays.asList
-    List<Grade> StudentGrades = new ArrayList<>(Arrays.asList(
-            new Grade("Gagan", "Potions", "C-"),
-            new Grade("Anshuman", "Potions", "B-"),
-            new Grade("Megha", "Potions", "Z-")
-    ));
+    List<Grade> StudentGrades = new ArrayList<>();
+
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        StudentGrades.add(grade);
+        return "redirect:/grades";
+    }
+    
+    
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        
-      
 
         model.addAttribute("grades", StudentGrades);
         return "grades";
